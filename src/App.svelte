a039f3097d93bee45861aad22468fe8d2d7759b0
Dmitry <3ddesign@bk.ru>
2020-06-09 08:28:46 +0300

    add binding example

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
-	export let name;
+	export let name = 'test';
+	export let counter = 0;
+
+	function increment() {
+		counter += 1;
+	} 
 </script>
 
 <style>
@@ -8,4 +13,5 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>{name}, {counter} </h1>
+<button on:click="{ increment }"> Increment </button>

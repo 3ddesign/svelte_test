88d43de02dc2deb0ca5e8296be5d9080d4c33971
Dmitry <3ddesign@bk.ru>
2020-06-24 08:55:29 +0300

    init app

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,9 @@
 <script>
-  import ContactCard from './ContactCard.svelte';
-
-  let name = 'Dmitry';
-  let title = '';
-  let image = '';
-  let description = '';
-  let formState = '';
-  let done = false;
-
-  let createdContacts = [];
-
-  function addContact(event) {
-    // event.preventDefault;
-    if (name.trim().length === 0 
-    || title.trim().length === 0 
-    || image.trim().length === 0 
-    || description.trim().length === 0) {
-      formState = 'invalid';
-      return;
-    }
-     createdContacts = [ ...createdContacts, { id: Math.random(), name: name, jobTitle: title, imageUrl: image, desc: description }]; 
-	   formState = 'done';
-  }
-
-  function deleteLast() {
-    createdContacts = createdContacts.slice(0, - 1);
-  }
 
 </script>
 
 <style>
-  #form {
-    width: 30rem;
-    max-width: 100%;
-    margin: 1rem 0;
-  }
+
 </style>
 
-<form id="form">
-  <div class="form-control">
-    <label for="userName">User Name</label>
-    <input type="text" bind:value={name} id="userName" />
-  </div>
-  <div class="form-control">
-    <label for="jobTitle">Job Title</label>
-    <input type="text" bind:value={title} id="jobTitle" />
-  </div>
-  <div class="form-control">
-    <label for="image">Image URL</label>
-    <input type="text" bind:value={image} id="image" />
-  </div>
-  <div class="form-control">
-    <label for="desc">Description</label>
-    <textarea rows="3" bind:value={description} id="desc" />
-  </div>
-<button on:click|preventDefault={addContact} type="submit">Add contact Card</button>
-</form>
-
-<button on:click="{event => createdContacts = createdContacts.slice(0, - 1)}">Delete First</button>
-<button on:click|once={deleteLast}>Delete Last</button>
-
-{#if formState === 'invalid'}
-<p>Invalid Input</p>
-{:else}
-<p>Plese enter some data</p>
-{/if} 
-
-{#each createdContacts as contact, i (contact.id)}
-<h2># {i + 1}</h2>
-<ContactCard userName={contact.name} jobTitle={contact.jobTitle} description={contact.desc} userImage={contact.imageUrl} />
-{:else}
-<p>No data</p>
-{/each}
+<!-- <h1></h1> -->
